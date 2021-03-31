4624b63e405d4e0f80dd71b77d4159f9c1f20a3b
umalogy <79102753+umalogy@users.noreply.github.com>
2021-03-31 23:38:39 +0800

    Add files via upload

---
+++ getvote.js
@@ -0,0 +1,11 @@
+
+function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
+}
